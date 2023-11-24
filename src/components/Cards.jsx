cce37cc5f2bf84bcd16f66fd0afe46ac1ef0fbf9
Elodieguay <e.guayroso@gmail.com>
2023-11-24 11:48:47 +0100

    fix: fix bugs on responsive

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,3 +1,4 @@
+import React from 'react'
 import { useState } from "react";
 import { generateUserData } from "../services/datas";
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,10 +3,10 @@ import Filter from './Filter'
 
 const Navbar = ({setSelectedGender, setOrderByBirth}) => {
   return (
-    <div className=' md:h-1/4 px-2 md:px-16 pt-6 max-sm:w-screen'>
+    <div className=' md:h-1/4 px-2 md:px-16 pt-6 max-md:w-screen'>
         <div className='flex justify-between md:mx-20 px-4 '>
           <img src='netatmologo.jpg'></img>
-          <h2 className='pt-2 text-lg'>Hello admin !</h2>
+          <h2 className='pt-2 text-lg'>Hi admin !</h2>
         </div>
         <div className='mb-4 px-3'>
           <Filter setSelectedGender= {setSelectedGender} setOrderByBirth={setOrderByBirth}/>
